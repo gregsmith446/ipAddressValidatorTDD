2f84bdb9552683e33048090cc1ffa4cb53e25eec
Greg <gregsmith446@gmail.com>
2019-08-11 00:37:31 -0400

    working code with accompanying test cases

---
+++ ipAddressValidatorClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ipAddressValidatorClass
+{
+    public class Validator
+    {
+        public bool ValidateIpv4Address(string address)
+        {
+            string[] digits = address.Split(".");
+
+            if (digits.Length != 4)
+            {
+                return false;
+            }
+            else if (digits.Length == 4 && digits[3] == "255")
+            {
+                return false;
+            }
+            else if (digits.Length == 4 && digits[3] == "0")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ipAddressValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using ipAddressValidatorClass;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ipAddressValidatorTests
+{
+    [TestFixture]
+    public class IpAddressValidatorTest
+    {
+        [Test]
+        public void GivenIPaddressWithLengthOf4ReturnsTrue()
+        {
+            string address = "192.168.1.1";
+
+            bool expected = true;
+            bool output = new Validator().ValidateIpv4Address(address);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void GivenIPaddressWithLengthLessThan4ReturnsFalse()
+        {
+            string address = "10.0.1";
+
+            bool expected = false;
+            bool output = new Validator().ValidateIpv4Address(address);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void GivenIPaddressEndingIn255ReturnsFalse()
+        {
+            string address = "192.168.1.255";
+
+            bool expected = false;
+            bool output = new Validator().ValidateIpv4Address(address);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void GivenIPaddressEndingIn0ReturnsFalse()
+        {
+            string address = "192.168.1.0";
+
+            bool expected = false;
+            bool output = new Validator().ValidateIpv4Address(address);
+
+            Assert.AreEqual(expected, output);
+        }
+
+    }
+}
